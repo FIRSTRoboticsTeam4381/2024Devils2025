f6c2d4f756cd9590e55e09cccc36b7e0ba43ad9c
Ella <Ella@leathermans.net>
2025-01-25 15:52:44 -0500

    updating commands

--- src/main/java/frc/robot/commands/CommandsAsWell.java
+++ src/main/java/frc/robot/commands/CommandsAsWell.java
@@ -4,27 +4,23 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Pivot;
+import frc.robot.subsystems.Swerve;
 
 /** Add your docs here. */
 public class CommandsAsWell 
 {
- ParallelCommandGroup(
-    pivot.Obtaining_Loop_Thingy,
-    Intake.take
- )
  
-
-
+ public final Pivot pivot = new Pivot();
 
 
 
 
 
 }
-
 
 
 
